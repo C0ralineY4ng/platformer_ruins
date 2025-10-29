f57b5fd7b99a6fd31172c00a757816a1c3cb6e4e
C0ralineY4ng <amandadalmeida2003@outlook.com>
2025-10-29 20:25:50 -0300

    Reorganização do projeto: estrutura, README e requirements adicionados

---
+++ README.md
@@ -0,0 +1,17 @@
+# Platformer Ruins 🎮
+
+Um jogo de plataforma 2D feito em **Python + Pygame Zero**, desenvolvido para fins de estudo e aprendizado.
+
+## 🚀 Como rodar o jogo
+
+1. Crie e ative o ambiente virtual:
+  
+   python -m venv venv
+   venv\Scripts\activate  # no Windows
+
+2.Instale as dependências: 
+    
+    pip install -r requirements.txt 
+
+3.Rode o jogo: 
+   python -m pgzero platformer_ruins/platformer.py" 
--- mecanica/config.py
+++ mecanica/config.py
@@ -1,36 +1,39 @@
+'''# Cores do menu clicável 
+COLOR_BACKGROUND = "black"
+COLOR_BUTTON = "blue"
+COLOR_TEXT = "white"'''
+
 # mecanica/config.py
 
 # Dimensões da tela
 WIDTH = 800
 HEIGHT = 600
 
-# Arquivo da imagem de fundo
+# Plano de fundo / sky
 BACKGROUND_IMAGE = "ruins.jpg"
-SKY = "sky"
-# Cor alternativa (fallback, caso a imagem não seja encontrada)
+SKY = "sky.png"  
+
+# Cor alternativa (fallback)
 BACKGROUND_COLOR = (209, 213, 216)
 
-
-# Cores do menu clicável 
-COLOR_BACKGROUND = "black"
-COLOR_BUTTON = "blue"
-COLOR_TEXT = "white"
+# Cores do menu
+COLOR_BACKGROUND = (0, 0, 0)   # preto
+COLOR_BUTTON = (0, 0, 255)     # azul
+COLOR_TEXT = (255, 255, 255)   # branco
 
 # Tiles e chão
 GROUND_TILE = "ground.png"
 PLATFORM_TILE = "platform.png"
 
-# Arquivo da imagem do player
+# Arquivos do player
 PLAYER_IDLE = "idle.png"
-PLAYER_RUN = ["run1.png,run2.png,run3.png"]
+PLAYER_RUN = ["run1.png", "run2.png", "run3.png"]
 PLAYER_JUMP = "jump.png"
-PLAYER_LOW = "lowrered.png"
+PLAYER_LOW = "lowered.png"  
 
-
-# Arquivo da imagem dos enemies
-
+# Arquivos dos inimigos
 GHOST = {
-    "idle": ["ghost_normal.png"],  # lista para expandir animações
+    "idle": ["ghost_normal.png"],
     "dead": ["ghost_dead.png"]
 }
 
